2a2acdc90288140436b5878543775e3d5fc9fbfe
Abdulla <a.muhammadjonov@newuu.uz>
2023-11-14 21:15:07 +0500

    Berdiyev

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -3,8 +3,8 @@ float problemSolution2(float i, float j, float k) {
     float result;
     if (i>j && i>k)
         result=i;
-    if else (j>i && j>k)
-    result=j;
+    if (j>i && j>k)
+        result=j;
     else
         result=k;
     return result;
--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,25 +1,25 @@
 #include <string>
 using namespace std;
 
- string problemSolution3  (float height, char S) {
+string problemSolution3  (float height, char S) {
 
-     if (S=='M'){
-         if (height<1.70)
-             return "Short";
-         else if (height>=1.85)
-             return "Tall";
-         else
-             return 'Normal";
+    if (S=='M'){
+        if (height<1.70)
+            return "Short";
+        else if (height>=1.85)
+            return "Tall";
+        else
+            return "Normal";
 
-     } else {
+    } else {
 
-             if (height<1.60)
-                 return "Short";
-             else if (height>=1.75)
-                 return "Tall";
-             else
-                 return 'Normal";
+        if (height<1.60)
+            return "Short";
+        else if (height>=1.75)
+            return "Tall";
+        else
+            return "Normal";
 
-     }
+    }
 
 }
